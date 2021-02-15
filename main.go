377946f67768ba7f12989c4c80fe9f1b2ca91648
Matias Rozanec <rozanecm@gmail.com>
2021-02-15 09:49:08 -0300

    4 nodes running - 1 controller to rule them all
    En el docker compose ahora se levantan 4 nodos. De ellos, el unico que controla es el nodo cuyo id es 1. Este nodo corre un chron job que cada x segundos chequea mediante docker ps si los nodos que debe controlar (los nombres los tiene almacenados en un array), estan vivos o no. Como esto es medio hacer trampa (porque docker
    'centraliza' de alguna forma el sistema distribuido, lo que hay que hacer es sacar el docker ps y hacer que los nodos se comuniquen de otra forma. Si bien podriamos usar rabbit para esto, no seria la herramienta adecuada (es un ultra massive overkill). Ahora lo que hay que hacer es usar sockets o http.

--- main.go
+++ main.go
@@ -2,53 +2,92 @@ package main
 
 import (
 	"context"
-	"io"
+//	"io"
+    "fmt"
 	"os"
 
 	"github.com/docker/docker/api/types"
-	"github.com/docker/docker/api/types/container"
+//	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/client"
-	"github.com/docker/docker/pkg/stdcopy"
+//	"github.com/docker/docker/pkg/stdcopy"
+
+    "github.com/jasonlvhit/gocron"
+//    "time"
 )
 
 func main() {
+    my_id := os.Getenv("ID")
+    fmt.Printf("My id: %s\n", my_id)
+//    startContainer("ubuntu")
+    gocron.Start()
+    if (my_id == "1") {
+        gocron.Every(5).Second().Do(routineCheck)
+    }
+//    time.Sleep(30 * time.Second)
+    while (true){}
+
+}
+
+func routineCheck() {
+    // for name in array with names: if not in docker ps, then start its container.
+	cli, err := client.NewEnvClient()
+	if err != nil {
+		panic(err)
+	}
+
+	containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{})
+	if err != nil {
+		panic(err)
+	}
+    fmt.Printf("%T\n", containers)
+
+    var ids = []string{"/node1", "/node2", "/node3", "/node4"}
+    for _, v := range ids {
+        fmt.Printf("checking container: %s\n", v)
+        if notRunning(v, containers) {
+            startContainer(v)
+        }
+	}
+    /*
+    for _, v := range containers {
+        fmt.Printf("%s\n", v.Names[0])
+    }
+    */
+}
+
+//func notRunning(container string, containers ) {
+// do stuff
+//}
+func notRunning(container string, containers []types.Container) bool {
+    for _, currentContainer := range containers {
+        if currentContainer.Names[0] == container {
+            return false
+        }
+    }
+    return true
+}
+
+func startContainer(name string) {
+    name = name[1:]
+    fmt.Printf("About to start container: %s\n", name)
     ctx := context.Background()
     cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
     if err != nil {
         panic(err)
     }
 
-    reader, err := cli.ImagePull(ctx, "docker.io/library/alpine", types.ImagePullOptions{})
+    /*
+     resp, err := cli.ContainerCreate(ctx, &container.Config{
+        Image: name,
+//        Cmd:   []string{"echo", "hello world"},
+    }, nil, nil, nil, name)
     if err != nil {
         panic(err)
     }
-    io.Copy(os.Stdout, reader)
+    */
 
-    resp, err := cli.ContainerCreate(ctx, &container.Config{
-        Image: "alpine",
-        Cmd:   []string{"echo", "hello world"},
-    }, nil, nil, nil, "")
-    if err != nil {
+//    if err := cli.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
+    if err := cli.ContainerStart(ctx, name, types.ContainerStartOptions{}); err != nil {
         panic(err)
     }
-
-    if err := cli.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
-        panic(err)
-    }
-
-    statusCh, errCh := cli.ContainerWait(ctx, resp.ID, container.WaitConditionNotRunning)
-    select {
-    case err := <-errCh:
-        if err != nil {
-            panic(err)
-        }
-    case <-statusCh:
-    }
-
-    out, err := cli.ContainerLogs(ctx, resp.ID, types.ContainerLogsOptions{ShowStdout: true})
-    if err != nil {
-        panic(err)
-    }
-
-    stdcopy.StdCopy(os.Stdout, os.Stderr, out)
-}
+ }
