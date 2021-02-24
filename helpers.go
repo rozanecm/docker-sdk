91f72a18d34768ffb86445b49e9faff8f96a1737
Matias Rozanec <rozanecm@gmail.com>
2021-02-24 12:05:52 -0300

    add leader election on node startup. Case of leader dying not being handled yet

--- helpers.go
+++ helpers.go
@@ -11,24 +11,30 @@ func getNamesOfNodesToControl() []string {
 	nodesToControl := os.Getenv("NODES_TO_CONTROL")
 	nodesToControl = strings.ReplaceAll(nodesToControl, "\"", "")
 	nodesToControlList := strings.Split(nodesToControl, " ")
-	fmt.Printf("Nodes to control: %s\n", nodesToControlList)
+	//fmt.Printf("Nodes to control: %s\n", nodesToControlList)
 	return nodesToControlList
 }
 
-func iAmLeader(id string) bool {
-	//TODO hacer esto como corresponde
-	return id == "1"
+func getControlSystemNodeNames() []string {
+	controlSystemNodes := os.Getenv("CONTROL_SYSTEM_NODES")
+	controlSystemNodes = strings.ReplaceAll(controlSystemNodes, "\"", "")
+	controlSystemNodesList := strings.Split(controlSystemNodes, " ")
+	//fmt.Printf("Control system Nodes: %s\n", controlSystemNodesList)
+	return controlSystemNodesList
 }
 
-func routineCheck(nodeNames []string, myId string) {
-	if !iAmLeader(myId){
+func routineCheck(nodeNames []string, controlSystemNodeNames []string, iAmLeader bool) {
+	if !iAmLeader {
 		return
 	}
 	for _, containerName := range nodeNames {
+		if containerName == os.Getenv("NAME") {
+			continue
+		}
 		fmt.Printf("checking container: %s\n", containerName)
 		currentURL := "http://" + containerName + ":8080/statusCheck"
 		_, err := http.Get(currentURL)
-		if err != nil{
+		if err != nil {
 			//fmt.Printf("error detected: %s", err)
 			fmt.Printf("Container %s detected as not running\n", containerName)
 			startContainer(containerName)
--- http.go
+++ http.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
-func initHttpServer() {
+func initHttpServer(iAmLeader *bool) {
 	http.HandleFunc("/statusCheck", statusCheckHandler)
-	go func() { log.Fatal(http.ListenAndServe(":8080", nil)) } ()
+	http.HandleFunc("/election", electionHandler)
+	http.HandleFunc("/leader", leaderHandler(iAmLeader))
+	go func() { log.Fatal(http.ListenAndServe(":8080", nil)) }()
 }
 
 func statusCheckHandler(writer http.ResponseWriter, request *http.Request) {
@@ -16,4 +20,35 @@ func statusCheckHandler(writer http.ResponseWriter, request *http.Request) {
 	msgJSON, _ := json.Marshal(msg)
 	writer.WriteHeader(200)
 	_, _ = writer.Write(msgJSON)
+}
+
+func electionHandler(writer http.ResponseWriter, request *http.Request) {
+	fmt.Println("election msg received.")
+	//msg := map[string]string{"Status": "Ok"}
+	//msgJSON, _ := json.Marshal(msg)
+	//writer.WriteHeader(200)
+	//_, _ = writer.Write(msgJSON)
+}
+
+func leaderHandler(iAmLeader *bool) http.HandlerFunc {
+	return func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Println("&&&&&&&&&&&&&&&&&&&&&leader msg received.")
+		type ExpectedResponse struct {
+			Leader string `json:"Leader"`
+		}
+		var eR ExpectedResponse
+		// Try to decode the request body into the struct. If there is an error,
+		// respond to the client with the error message and a 400 status code.
+		err := json.NewDecoder(request.Body).Decode(&eR)
+		if err != nil {
+			http.Error(writer, err.Error(), http.StatusBadRequest)
+			return
+		}
+		fmt.Printf("**** -> New leader received: %s %s \n", eR.Leader, eR)
+		*iAmLeader = false
+		if eR.Leader < os.Getenv("NAME") {
+			fmt.Println("-> Someone wants to be leader, but I should be!")
+			election(nil)
+		}
+	}
 }
