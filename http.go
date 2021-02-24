7836ba7e3e1573cfb86049334d4abbc687dee56a
Matias Rozanec <rozanecm@gmail.com>
2021-02-24 14:11:20 -0300

    refactor and env var test
    - move election logic to its own file
    - check env vars behaviour.

--- http.go
+++ http.go
@@ -10,8 +10,9 @@ import (
 
 func initHttpServer(iAmLeader *bool) {
 	http.HandleFunc("/statusCheck", statusCheckHandler)
-	http.HandleFunc("/election", electionHandler)
+	http.HandleFunc("/election", electionHandler(iAmLeader))
 	http.HandleFunc("/leader", leaderHandler(iAmLeader))
+	http.HandleFunc("/test", testHandler)
 	go func() { log.Fatal(http.ListenAndServe(":8080", nil)) }()
 }
 
@@ -22,17 +23,35 @@ func statusCheckHandler(writer http.ResponseWriter, request *http.Request) {
 	_, _ = writer.Write(msgJSON)
 }
 
-func electionHandler(writer http.ResponseWriter, request *http.Request) {
-	fmt.Println("election msg received.")
-	//msg := map[string]string{"Status": "Ok"}
-	//msgJSON, _ := json.Marshal(msg)
-	//writer.WriteHeader(200)
-	//_, _ = writer.Write(msgJSON)
+func testHandler(writer http.ResponseWriter, request *http.Request) {
+	/* Test to receive som random env var
+	 * to see if they are available when a node is restarted.
+	 */
+	type ExpectedResponse struct {
+		Msg string `json:"First node to control"`
+	}
+	var eR ExpectedResponse
+	// Try to decode the request body into the struct. If there is an error,
+	// respond to the client with the error message and a 400 status code.
+	err := json.NewDecoder(request.Body).Decode(&eR)
+	if err != nil {
+		http.Error(writer, err.Error(), http.StatusBadRequest)
+		return
+	}
+	fmt.Printf("**** -> New test msg received: %s \n", eR.Msg)
+}
+
+func electionHandler(iAmLeader *bool) http.HandlerFunc {
+	return func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Println("election msg received.")
+		election(iAmLeader)
+	}
+
 }
 
 func leaderHandler(iAmLeader *bool) http.HandlerFunc {
 	return func(writer http.ResponseWriter, request *http.Request) {
-		fmt.Println("&&&&&&&&&&&&&&&&&&&&&leader msg received.")
+		fmt.Println("&&&&&&&&&&&&&&&&&&&&& leader msg received.")
 		type ExpectedResponse struct {
 			Leader string `json:"Leader"`
 		}
@@ -48,7 +67,7 @@ func leaderHandler(iAmLeader *bool) http.HandlerFunc {
 		*iAmLeader = false
 		if eR.Leader < os.Getenv("NAME") {
 			fmt.Println("-> Someone wants to be leader, but I should be!")
-			election(nil)
+			election(iAmLeader)
 		}
 	}
 }
--- main.go
+++ main.go
@@ -3,21 +3,16 @@ package main
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"
 	"github.com/jasonlvhit/gocron"
 	"net/http"
-	"os"
-	"time"
 )
 
 const interval = 5
 
 func main() {
+	test()
 	iAmLeader := false
 	initHttpServer(&iAmLeader)
-	if os.Getenv("NAME") == "node4" {
-		time.Sleep(5000)
-	}
 	election(&iAmLeader)
 	gocron.Start()
 	_ = gocron.Every(interval).Second().Do(routineCheck, getNamesOfNodesToControl(), getControlSystemNodeNames(), iAmLeader)
@@ -25,61 +20,13 @@ func main() {
 	}
 }
 
-func election(iAmLeader *bool) {
-	iAmNewLeader := startElection(getControlSystemNodeNames())
-	if iAmNewLeader {
-		fmt.Printf("%d - I, %s, am new leader\n", time.Now().UnixNano(), os.Getenv("NAME"))
-		*iAmLeader = true
-		announceNewLeadership(getControlSystemNodeNames())
-	}
-}
-
-func announceNewLeadership(controlSystemNodeNames []string) {
-	//	TODO implement this.
-	msg := map[string]string{"Leader": os.Getenv("NAME")}
-	msgJSON, _ := json.Marshal(msg)
-	for _, node := range controlSystemNodeNames {
-		if node == os.Getenv("NAME") {
-			continue
-		}
-		url := "http://" + node + ":8080/leader"
-		fmt.Printf("Announcing new leader to %s\n", node)
-		_, err := http.Post(url, "application/json", bytes.NewBuffer(msgJSON))
-		if err != nil {
-			fmt.Printf("Error sending leader: %s\n", err)
-		}
-
-	}
-}
-
-func startElection(controlSystemNodeNames []string) bool {
-	/* In the election process, one sends an election msg to all
-	 * bigger nodes in the system.
-	 * If any of them responds, it will continue the election process.
-	 * If none of them respond, then the current node is the new leader.
-	 * The return statement is the negation of the bool because if no one has answered, then this node is the leader.
+func test() {
+	/* Test to receive som random env var
+	 * to see if they are available when a node is restarted.
 	 */
-	fmt.Println("In election")
-	hasAnyBiggerNodeResponded := false
-	for _, node := range controlSystemNodeNames {
-		if os.Getenv("NAME") >= node {
-			continue
-		}
-		fmt.Printf("In election with %s\n", node)
-		url := "http://" + node + ":8080/election"
-		fmt.Printf("Sending election msg to %s\n", node)
-		_, err := http.Get(url)
-		if err != nil {
-			//fmt.Printf("error detected: %s", err)
-			fmt.Printf("Container %s detected as not running\n", node)
-		} else {
-			hasAnyBiggerNodeResponded = true
-			/* If a bigger node responded, then there's no need
-			 * to send extra messages to other bigger nodes.
-			 * This node will continue with the election anyway.
-			 */
-			break
-		}
-	}
-	return !hasAnyBiggerNodeResponded
+	msg := map[string]string{"First node to control": getControlSystemNodeNames()[0]}
+	msgJSON, _ := json.Marshal(msg)
+
+	url := "http://" + "node4" + ":8080/test"
+	_, _ = http.Post(url, "application/json", bytes.NewBuffer(msgJSON))
 }
